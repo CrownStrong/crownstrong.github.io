fecd318b9f6b2ee953c578e764d28fef5af55c75
CrownStrong <71077959+CrownStrong@users.noreply.github.com>
2024-02-27 07:53:46 +0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,16 +4,17 @@ import { FieldBuilder } from "./field.js";
 const Field = new FieldBuilder;
 const GAME = new Game;
 const config = {
-	columns: 10,
-	rows: 10,
-	speed: 400,
+	columns: 100,
+	rows: 100,
+	speed: Number(document.getElementById('speed').value),
 }
 const gameElement = document.querySelector('.game-wrap');
+// let timerHold = null;
+let configureOpened = false;
 
 
 function changeConfig(ev){
 	ev.preventDefault();
-	config.speed = Number(document.getElementById('speed').value);
 	config.columns = Number(document.getElementById('columns').value);
 	config.rows = Number(document.getElementById('rows').value);
 	buildField();
@@ -25,16 +26,18 @@ function buildField(){
 }
 
 function playPause(ev){
-	document.getElementById('configure').classList.toggle('disabled', !GAME.played);
+	document.getElementById('fieldSubmit').toggleAttribute('disabled', !GAME.played);
+	document.getElementById('columns').toggleAttribute('disabled', !GAME.played);
+	document.getElementById('rows').toggleAttribute('disabled', !GAME.played);
 	document.getElementById('randomizeButton').toggleAttribute('disabled', !GAME.played);
 	document.getElementById('nextStepButton').toggleAttribute('disabled', !GAME.played);
 	document.getElementById('resetButton').toggleAttribute('disabled', !GAME.played);
 	gameElement.classList.toggle('blocked', !GAME.played);
 	ev.target.classList.toggle('paused', GAME.played);
-	GAME.playClickHandler.bind(GAME)();
+	GAME.playClickHandler.call(GAME);
 }
 
-document.getElementById('configure').addEventListener('submit', changeConfig);
+document.getElementById('configureForm').addEventListener('submit', changeConfig);
 
 buildField();
 
@@ -44,6 +47,19 @@ document.getElementById('playPause').addEventListener(
 gameElement.addEventListener(
 	'click', GAME.clickItemHandler.bind(GAME)
 );
+
+// gameElement.addEventListener(
+// 	'mousemove', ev=>{
+// 		if(ev.buttons == 1 && !timerHold){
+// 			GAME.clickItemHandler.call(GAME, ev)
+// 			timerHold = setTimeout(()=>{
+// 				clearTimeout(timerHold);
+// 				timerHold = null;
+// 			}, 100);
+// 		}
+// 	}
+// );
+
 document.getElementById('nextStepButton').addEventListener(
 	'click', GAME.changeItem.bind(GAME)
 );
@@ -57,3 +73,19 @@ document.getElementById('randomizeButton').addEventListener(
 		Field.randomize()
 	}
 );
+document.getElementById('configureButton').addEventListener(
+    'click', ()=>{
+        configureOpened = !configureOpened;
+        document.getElementById('configure').classList.toggle(
+            'opened', configureOpened
+        )
+    }
+)
+document.getElementById('speed').addEventListener(
+    'change', ev=>{
+        GAME.changeSpeed.call(
+            GAME, 
+            Number(ev.target.value)
+        )
+    }
+)
